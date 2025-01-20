43e2d54c4c216f964af0550d087455906e287940
pettta <tp663@drexel.edu>
2025-01-20 18:01:15 -0500

    remove build folders from repo

--- mac/vulkan_tutorial/vtut_triangles_no_raii/build/CMakeFiles/vtut_triangles_no_raii.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for vtut_triangles_no_raii.
--- mac/vulkan_tutorial/vtut_triangles_no_raii/build/CMakeFiles/vtut_triangles_no_raii.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for vtut_triangles_no_raii.
-# This may be replaced when dependencies are built.
