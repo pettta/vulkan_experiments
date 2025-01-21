721d983920c438e5b680c45b374afe83135941e5
pettta <tp663@drexel.edu>
2025-01-20 20:00:25 -0500

    finishing up devices_validation_messages folder

--- mac/vulkan_tutorial/devices_validation_messages/main.cpp
+++ mac/vulkan_tutorial/devices_validation_messages/main.cpp
@@ -5,13 +5,11 @@
 #include <stdexcept>
 #include <cstdlib>
 
-class HelloTriangleApplication {
-private:  GLFWwindow* window; // holds pointer to GLFWwindow
-private: VkInstance instance; // holds instance of connection between app and vulkan
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 // Validation Layer setup - preprocessor directive 
+// Allows us to specify at build time whether we want the debugger we will define below to actually log 
 const std::vector<const char*> validationLayers = {
     "VK_LAYER_KHRONOS_validation"
 };
@@ -23,6 +21,26 @@ const std::vector<const char*> validationLayers = {
 // End Validation Layer preprocessor directive 
 
 
+//Debugger creation and destruction external definitions 
+//Anything that is not core to vulkan must be loaded like this 
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+    auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+    } else {
+        return VK_ERROR_EXTENSION_NOT_PRESENT;
+    }
+}
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+    auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+    if (func != nullptr) {
+        func(instance, debugMessenger, pAllocator);
+    }
+}
+
+
+
+class HelloTriangleApplication {
 public:
     void run() {
         // GLFW Calls 
@@ -34,7 +52,24 @@ public:
     }
 
 private:
+GLFWwindow* window; // holds pointer to GLFWwindow
+VkInstance instance; // holds instance of connection between app and vulkan
+VkDebugUtilsMessengerEXT debugMessenger; // holds instance of debug manager that can talk to vulkan 
 
+    /* Extensions and Validation Layers (Optional)*/
+    std::vector<const char*> getRequiredExtensions() {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        if (enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        return extensions;
+    }
     bool checkValidationLayerSupport() {
         uint32_t layerCount;
         vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
@@ -56,7 +91,23 @@ private:
         }
         return true;
     }
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+        //VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT: Diagnostic message
+        //VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT: Informational message like the creation of a resource
+        //VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT: Message about behavior that is not necessarily an error, but very likely a bug in your application
+        //VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT: Message about behavior that is invalid and may cause crashes
+        if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+            // Message is important enough to show
+            std::cerr << "I Should be logging here" << std::endl; // TODO IMPLEMENT AT SOME POINT 
+            std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+        }
+        return VK_FALSE;
+    }
+    /*End Validation/Message Related optional Material */
 
+
+
+    /* Foundational Methods */
     void initWindow() {
         // Start glfw
         glfwInit();
@@ -92,28 +143,40 @@ private:
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
 
+
         // Not optional struct that tells the Vulkan driver which global extensions and validation layers we want to use
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
+
+        
+        // Optional Debugger(adding it to the next of create info allows us to debug the debugger having issues)
+        // Plus which global validation layers to enable 
+        // See https://github.com/KhronosGroup/Vulkan-Docs/blob/main/appendices/VK_EXT_debug_utils.adoc#examples
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+            debugCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+            debugCreateInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+            debugCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+            debugCreateInfo.pfnUserCallback = debugCallback;
+            debugCreateInfo.pUserData = nullptr; 
+            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
+        } else {
+            createInfo.enabledLayerCount = 0;
+
+            createInfo.pNext = nullptr;
+        }
 
         // specification of global extensions so vulkan can talk with glfw 
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
         createInfo.enabledExtensionCount = glfwExtensionCount;
-        createInfo.ppEnabledExtensionNames = glfwExtensions;
 
-        // Which global validation layers to enable -- depends on which mode you are on  
-        if (enableValidationLayers) {
-            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-            createInfo.ppEnabledLayerNames = validationLayers.data();
-        } else {
-            createInfo.enabledLayerCount = 0;
-        }
         
-
-        // Regarding Extensions -- Info about what vulkan supports(completely optional, CAN BE REMOVED SAFELY!!!)
+        // Extensions -- Info about what vulkan supports(completely optional, CAN BE REMOVED SAFELY!!!)
         uint32_t extensionCount = 0;
         vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
         std::vector<VkExtensionProperties> extensions(extensionCount);
@@ -123,23 +186,29 @@ private:
             std::cout << '\t' << extension.extensionName << '\n';
         }
 
-
-        // Regarding Extensions -- necessities for moltenvk to connect metal and vulkan (MACOS ONLY REQUIREMENT!!)
+        // Extensions 
         std::vector<const char*> requiredExtensions;
         for(uint32_t i = 0; i < glfwExtensionCount; i++) {
             requiredExtensions.emplace_back(glfwExtensions[i]);
         }
+        //necessities for debugger to connect to createinfo above 
+        requiredExtensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        //necessities for moltenvk to connect metal and vulkan (MACOS ONLY REQUIREMENT!!)
         requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
         createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
         createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
         createInfo.ppEnabledExtensionNames = requiredExtensions.data();
 
+
         // Create vulkan instance, check everything worked out 
-        //VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
             throw std::runtime_error("failed to create instance!");
         }
 
+        // Create debugger instance
+        if (CreateDebugUtilsMessengerEXT(instance, &debugCreateInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+            throw std::runtime_error("failed to set up debug messenger!");
+        }
     }
 
     void mainLoop() {
@@ -151,6 +220,9 @@ private:
 
     void cleanup() {
         // reverse-order of creation 
+        if (enableValidationLayers) {
+            DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);m
+        }
         vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
--- mac/vulkan_tutorial/readme.md
+++ mac/vulkan_tutorial/readme.md
@@ -2,11 +2,12 @@
 
 # (1) Basic Foundation
 
-- Provides the very basic skeleton 
+- Provides the very basic skeleton of a window using GLFW 
 
 # (2) devices_validation_messages 
 
 - Provides instances, message handling, and validation layers 
+- Is able to catch on debug mode when devices are created but not destroyed 
 
 # (3) phys_log_devices_swapchains 
 
