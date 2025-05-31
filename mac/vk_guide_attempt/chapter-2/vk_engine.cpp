0c4c08ceec99fdc5e7f829a432a263014ea333da
pettta <tp663@drexel.edu>
2025-05-30 22:13:31 -0400

    added some chapter-2 of vkguide stuff: deletion queue to handle cleaning up new objects, VMA (plus getting it to work with volk), etc..

---
+++ mac/vk_guide_attempt/chapter-2/vk_engine.cpp
@@ -0,0 +1,356 @@
+﻿//> includes 
+// -- Stdlb -- //
+#include <chrono>
+#include <thread>
+
+// -- Inside folder -- // 
+#include "vk_engine.h"
+
+
+// -- Extrenal 
+//#include "volk.h"
+#define VK_NO_PROTOTYPES
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+
+#define VMA_IMPLEMENTATION
+#define VMA_STATIC_VULKAN_FUNCTIONS 0 
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1 
+#include "vk_mem_alloc.h"
+#include "vk_initializers.h"
+#include "vk_types.h"
+#include "vk_images.h"
+
+#include "VkBootstrap.h"
+//< includes
+
+//> init
+constexpr bool bUseValidationLayers = false;
+
+VulkanEngine* loadedEngine = nullptr;
+
+VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
+void VulkanEngine::init()
+{
+    // Init SDL, create window 
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+    _window = SDL_CreateWindow(
+        "Vulkan Engine",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        _windowExtent.width,
+        _windowExtent.height,
+        window_flags);
+
+    // Build out app 
+    init_vulkan(); 
+    init_swapchain(); 
+    init_commands(); 
+    init_sync_structures();
+
+    // everything went fine
+    _isInitialized = true;
+}
+
+void VulkanEngine::init_vulkan()
+{
+    volkInitialize(); // Must check return value!
+	/** Use vkbootstrap to create a Vulkan instance **/
+    
+    //make the vulkan instance, with basic debug features
+    vkb::InstanceBuilder builder;
+	auto inst_ret = builder.set_app_name("Example Vulkan Application")
+		.request_validation_layers(bUseValidationLayers)
+		.use_default_debug_messenger()
+		.require_api_version(1, 2, 0) // 1.2 rather than 1.3 on mac 
+		.build();
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	//grab the instance & debug messenger, need to store so we can prevent leaks 
+	_instance = vkb_inst.instance;
+	_debug_messenger = vkb_inst.debug_messenger;
+
+    /** Use vkbootstrap to create a surface and chose what device to use **/
+    // (MAC MOMENT! - This is the part that is different from the reference code since mac doesnt support 1.3 yet)
+    
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+	//vulkan 1.3 features(Not supported on mac)
+	// VkPhysicalDeviceVulkan13Features features{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+	// features.dynamicRendering = true;
+	// features.synchronization2 = true;
+
+	//vulkan 1.2 features
+	VkPhysicalDeviceVulkan12Features features12{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+	features12.bufferDeviceAddress = true;
+	features12.descriptorIndexing = true;
+
+
+    VkPhysicalDeviceSynchronization2Features sync2Features{};
+    sync2Features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SYNCHRONIZATION_2_FEATURES;
+    sync2Features.synchronization2 = true;
+    
+    features12.pNext=&sync2Features; // TODO MAYBE REMOVE? 
+    
+	//use vkbootstrap to select a gpu. 
+	//We want a gpu that can write to the SDL surface and supports vulkan 1.3 (but alas we are on mac, so add the same features manually)
+	vkb::PhysicalDeviceSelector selector{ vkb_inst };
+	vkb::PhysicalDevice physicalDevice = selector
+		.set_minimum_version(1, 2)
+        .add_required_extension(VK_KHR_DYNAMIC_RENDERING_EXTENSION_NAME)
+        .add_required_extension(VK_KHR_DEPTH_STENCIL_RESOLVE_EXTENSION_NAME)
+        .add_required_extension(VK_KHR_CREATE_RENDERPASS_2_EXTENSION_NAME)
+        .add_required_extension(VK_KHR_MULTIVIEW_EXTENSION_NAME)
+        .add_required_extension(VK_KHR_MAINTENANCE_2_EXTENSION_NAME)
+        .add_required_extension(VK_KHR_SYNCHRONIZATION_2_EXTENSION_NAME)
+        .set_required_features_12(features12)
+        .add_required_extension_features(sync2Features)
+		.set_surface(_surface)
+		.select()
+		.value();
+
+	//create the final vulkan device
+	vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	//Get the VkDevice handle used in the rest of a vulkan application
+	_device = vkbDevice.device;
+	_chosenGPU = physicalDevice.physical_device;
+
+    // Volk for device and instance 
+	volkLoadInstance(_instance); 
+	volkLoadDevice(_device);
+
+	/**Use VKbootstrap to init graphics queue and family  */
+    _graphicsQueue = vkbDevice.get_queue(vkb::QueueType::graphics).value(); 
+    _graphicsQueueFamily = vkbDevice.get_queue_index(vkb::QueueType::graphics).value();
+
+	// initialize VMA , set properties 
+    VmaAllocatorCreateInfo allocatorInfo = {};
+    allocatorInfo.physicalDevice = _chosenGPU;
+    allocatorInfo.device = _device;
+    allocatorInfo.instance = _instance;
+    allocatorInfo.flags = VMA_ALLOCATOR_CREATE_BUFFER_DEVICE_ADDRESS_BIT;
+	
+	// not in tutorial:  necessary for dynamic linking (which volk does for everything)
+	VmaVulkanFunctions vmaVulkanFunctions = {};
+	vmaVulkanFunctions.vkGetInstanceProcAddr = vkGetInstanceProcAddr;
+	vmaVulkanFunctions.vkGetDeviceProcAddr = vkGetDeviceProcAddr;
+	allocatorInfo.pVulkanFunctions = &vmaVulkanFunctions;
+
+    vmaCreateAllocator(&allocatorInfo, &_allocator);
+    _mainDeletionQueue.push_function([&]() {
+        vmaDestroyAllocator(_allocator);
+    });
+
+}
+void VulkanEngine::create_swapchain(uint32_t width, uint32_t height)
+{
+	vkb::SwapchainBuilder swapchainBuilder{ _chosenGPU,_device,_surface };
+
+	_swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM; // Colorspace 
+	vkb::Swapchain vkbSwapchain = swapchainBuilder
+		//.use_default_format_selection()
+		.set_desired_format(VkSurfaceFormatKHR{ .format = _swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR })
+		//use vsync present mode
+		.set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+		.set_desired_extent(width, height)
+		.add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+		.build()
+		.value();
+
+	_swapchainExtent = vkbSwapchain.extent;
+	//store swapchain and its related images
+	_swapchain = vkbSwapchain.swapchain;
+	_swapchainImages = vkbSwapchain.get_images().value();
+	_swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::init_swapchain()
+{
+	create_swapchain(_windowExtent.width, _windowExtent.height);
+}
+
+/**We will use VK init for the next two, but its basically 
+Create info struct --> fill attributes --> check everything works out **/
+
+void VulkanEngine::init_commands()
+{
+    // command pool info --> command buffer info --> verify
+    VkCommandPoolCreateInfo commandPoolInfo = vkinit::command_pool_create_info(_graphicsQueueFamily, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+    for (int i = 0; i < FRAME_OVERLAP; i++) {
+        // NOTE: this line below might be causing issues 
+		VK_CHECK(vkCreateCommandPool(_device, &commandPoolInfo, nullptr, &_frames[i]._commandPool));
+		// allocate the default command buffer that we will use for rendering
+		VkCommandBufferAllocateInfo cmdAllocInfo = vkinit::command_buffer_allocate_info(_frames[i]._commandPool, 1);
+		VK_CHECK(vkAllocateCommandBuffers(_device, &cmdAllocInfo, &_frames[i]._mainCommandBuffer));
+	}
+}
+void VulkanEngine::init_sync_structures()
+{
+    //fenceinfo --> sempaphore info --> verify 
+	//1 fence to control gpu when finished render frame, 2 semaphores to sync render with swapchain
+	//we want the fence to start signalled so we can wait on it on the first frame (VK_FENCE_CREATE_SIGNALED_BIT)
+	VkFenceCreateInfo fenceCreateInfo = vkinit::fence_create_info(VK_FENCE_CREATE_SIGNALED_BIT);
+	VkSemaphoreCreateInfo semaphoreCreateInfo = vkinit::semaphore_create_info();
+	for (int i = 0; i < FRAME_OVERLAP; i++) {
+		VK_CHECK(vkCreateFence(_device, &fenceCreateInfo, nullptr, &_frames[i]._renderFence));
+		VK_CHECK(vkCreateSemaphore(_device, &semaphoreCreateInfo, nullptr, &_frames[i]._swapchainSemaphore));
+		VK_CHECK(vkCreateSemaphore(_device, &semaphoreCreateInfo, nullptr, &_frames[i]._renderSemaphore));
+	}
+}
+//< init
+
+//> extras
+void VulkanEngine::destroy_swapchain()
+{
+	vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+	// destroy swapchain resources
+	for (int i = 0; i < _swapchainImageViews.size(); i++) {
+
+		vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+	}
+}
+
+void VulkanEngine::cleanup()
+{
+    // Cleanup in reverse order (throws errors otherwise due to leaks)
+    // Commands --> Sync --> Swapchain --> Surface --> Device --> Debug --> Instance --> Window
+    if (_isInitialized) {
+		vkDeviceWaitIdle(_device);
+		for (int i = 0; i < FRAME_OVERLAP; i++) {
+            // destroy command pool and sync objects
+			vkDestroyCommandPool(_device, _frames[i]._commandPool, nullptr);
+			vkDestroyFence(_device, _frames[i]._renderFence, nullptr);
+			vkDestroySemaphore(_device, _frames[i]._renderSemaphore, nullptr);
+			vkDestroySemaphore(_device ,_frames[i]._swapchainSemaphore, nullptr);
+			_frames[i]._deletionQueue.flush(); 
+        }
+		destroy_swapchain();
+		vkDestroySurfaceKHR(_instance, _surface, nullptr);
+		vkDestroyDevice(_device, nullptr);
+		vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+		vkDestroyInstance(_instance, nullptr);
+		SDL_DestroyWindow(_window);
+	}
+}
+
+void VulkanEngine::draw()
+{
+//> draw_1: wait until the gpu has finished rendering the last frame. Timeout of 1s
+	VK_CHECK(vkWaitForFences(_device, 1, &get_current_frame()._renderFence, true, 1000000000));
+	// VK_CHECK(vkResetFences(_device, 1, &get_current_frame()._renderFence));
+	get_current_frame()._deletionQueue.flush(); 
+//< draw_1
+
+//> draw_2: request image from the swapchain
+	uint32_t swapchainImageIndex;
+	VK_CHECK(vkAcquireNextImageKHR(_device, _swapchain, 1000000000, get_current_frame()._swapchainSemaphore, nullptr, &swapchainImageIndex));
+//< draw_2
+
+//> draw_3: begin recording the command buffer
+	VkCommandBuffer cmd = get_current_frame()._mainCommandBuffer;
+
+	// now that the cmds finished executing, we can safely reset the cmd buffer to begin recording again.
+	VK_CHECK(vkResetCommandBuffer(cmd, 0));
+
+	//begin the cmd buffer recording. We use this cmd buffer once, so we let vulkan know that
+	VkCommandBufferBeginInfo cmdBeginInfo = vkinit::command_buffer_begin_info(VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT);
+	VK_CHECK(vkBeginCommandBuffer(cmd, &cmdBeginInfo));
+//< draw_3
+
+//> draw_4 make swapchain img to writeable
+	vkutil::transition_image(cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL);
+
+	//make a clear-color from frame number. This will flash with a 120 frame period. Clear image 
+	VkClearColorValue clearValue;
+	float flash = std::abs(std::sin(_frameNumber / 120.f));
+	clearValue = { { 0.0f, 0.0f, flash, 1.0f } };
+	VkImageSubresourceRange clearRange = vkinit::image_subresource_range(VK_IMAGE_ASPECT_COLOR_BIT);
+	vkCmdClearColorImage(cmd, _swapchainImages[swapchainImageIndex], VK_IMAGE_LAYOUT_GENERAL, &clearValue, 1, &clearRange);
+
+	//make the swapchain image into presentable mode
+	vkutil::transition_image(cmd, _swapchainImages[swapchainImageIndex],VK_IMAGE_LAYOUT_GENERAL, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
+
+	//finalize the command buffer (we can no longer add commands, but it can now be executed)
+	VK_CHECK(vkEndCommandBuffer(cmd));
+//< draw_4
+
+//> draw_5 prepare the submission to the queue, then render and submit 
+	//we want to wait on the _presentSemaphore, as that semaphore is signaled when the swapchain is ready
+	//we will signal the _renderSemaphore, to signal that rendering has finished
+	VkCommandBufferSubmitInfo cmdinfo = vkinit::command_buffer_submit_info(cmd);	
+	VkSemaphoreSubmitInfo waitInfo = vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT_KHR,get_current_frame()._swapchainSemaphore);
+	VkSemaphoreSubmitInfo signalInfo = vkinit::semaphore_submit_info(VK_PIPELINE_STAGE_2_ALL_GRAPHICS_BIT, get_current_frame()._renderSemaphore);	
+	
+	VkSubmitInfo2 submit = vkinit::submit_info(&cmdinfo,&signalInfo,&waitInfo);	
+
+	//submit command buffer to the queue and execute it.
+	// _renderFence will now block until the graphic commands finish execution
+	// macos requires this be a KHR function, but the non KHR version would be used for windows, etc...
+	VK_CHECK(vkQueueSubmit2KHR(_graphicsQueue, 1, &submit, get_current_frame()._renderFence));
+//< draw_5
+
+//> draw_6 put img we just rendered to into the visible window.
+	// we want to wait on the _renderSemaphore for that, 
+	// as its necessary that drawing commands have finished before the image is displayed to the user
+	VkPresentInfoKHR presentInfo = {};
+	presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+	presentInfo.pNext = nullptr;
+	presentInfo.pSwapchains = &_swapchain;
+	presentInfo.swapchainCount = 1;
+
+	presentInfo.pWaitSemaphores = &get_current_frame()._renderSemaphore;
+	presentInfo.waitSemaphoreCount = 1;
+
+	presentInfo.pImageIndices = &swapchainImageIndex;
+
+	VK_CHECK(vkQueuePresentKHR(_graphicsQueue, &presentInfo));
+
+	//increase the number of frames drawn
+	_frameNumber++;
+
+//< draw_6
+
+}
+//< extras
+
+//> drawloop
+void VulkanEngine::run()
+{
+    SDL_Event e;
+    bool bQuit = false;
+
+    // main loop
+    while (!bQuit) {
+        // Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            // close the window when user alt-f4s or clicks the X button
+            if (e.type == SDL_QUIT)
+                bQuit = true;
+
+            if (e.type == SDL_WINDOWEVENT) {
+                if (e.window.event == SDL_WINDOWEVENT_MINIMIZED) {
+                    stop_rendering = true;
+                }
+                if (e.window.event == SDL_WINDOWEVENT_RESTORED) {
+                    stop_rendering = false;
+                }
+            }
+        }
+
+        // do not draw if we are minimized
+        if (stop_rendering) {
+            // throttle the speed to avoid the endless spinning
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        draw();
+    }
+}
+//< drawloop
