5dfaf8ef096e3233b60729469107d1a301ed67dd
pettta <tp663@drexel.edu>
2025-09-30 21:02:53 -0400

    render imgui demo so i know how to implement certain stuff going forward

--- os-independent/chapter-2/vk_engine.cpp
+++ os-independent/chapter-2/vk_engine.cpp
@@ -316,7 +316,7 @@ void VulkanEngine::run()
 			ImGui::InputFloat4("data2",(float*)& selected.data.data2);
 			ImGui::InputFloat4("data3",(float*)& selected.data.data3);
 			ImGui::InputFloat4("data4",(float*)& selected.data.data4);
-		
+			ImGui::ShowDemoWindow();
 			ImGui::End();
 		}
 		ImGui::Render();
